c073217398276bb7a8003e2bd17c272e1d73c30f
phamtanphat <phatdroid94@gmail.com>
2020-08-24 07:36:40 -0700

    export and export default

--- App.js
+++ App.js
@@ -2,111 +2,15 @@
 //imrc : import react , component
 
 import React, { Component } from 'react'
-import { Text, View, StyleSheet , SafeAreaView} from 'react-native'
+import { SafeAreaView} from 'react-native'
+import { MainScreen } from './src/screen/MainScreen'
 
 export default class App extends Component {
   render() {
     return (
-      <SafeAreaView
-        style={{
-          flex : 1,
-          flexDirection : 'column',
-        }}>
-        <View style={{flex : 1 , backgroundColor : 'green'}}>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : '#f5f5f5'
-            }}>
-            <Text >A</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'gray'
-            }}>
-            <Text >B</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'pink'
-            }}>
-            <Text >C</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'orange'
-            }}>
-            <Text >D</Text>
-          </View>
-        </View>
-        <View 
-          style={{
-            flex : 1 ,
-            backgroundColor : 'green',
-            flexDirection : 'row'
-          }}>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : '#f5f5f5'
-            }}>
-            <Text >A</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'gray'
-            }}>
-            <Text >B</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'pink'
-            }}>
-            <Text >C</Text>
-          </View>
-          <View 
-            style={{
-              flex : 1 , 
-              justifyContent : 'center', 
-              alignItems : 'center',
-              backgroundColor : 'orange'
-            }}>
-            <Text >D</Text>
-          </View>
-        </View>
+      <SafeAreaView style={{flex : 1}}>
+        <MainScreen1/>
       </SafeAreaView>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  textStyle: {
-    margin: 50,
-    color: 'red',
-    borderRadius: 5,
-    borderWidth: 2,
-    borderColor: 'blue',
-    padding: 10,
-    fontWeight: '500',
-    fontStyle: 'italic',
-  }
-})
