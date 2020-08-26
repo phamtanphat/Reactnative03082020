43953e56a3f2bfae10e23d7275c12d48c56c4909
phamtanphat <phatdroid94@gmail.com>
2020-08-26 07:35:59 -0700

    spread props object css

--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -1,47 +1,88 @@
 import React, { Component } from 'react'
-import { Text, View , StyleSheet} from 'react-native'
+import { Text, View, StyleSheet , TouchableOpacity} from 'react-native'
 import Box from '../components/Box'
 
 export default class MainScreen extends Component {
 
-    showVn = (word) => {
-        if(word.isMemorized){
-            return(
-                <Text style={styles.textVn}>----</Text>
-            )
-        }else{
-            return(
-                <Text style={styles.textVn}>{word.vn}</Text>
-            )
-        }
-    }
+    // showVn = (word) => {
+    //     if(word.isMemorized){
+    //         return(
+    //             <Text style={styles.textVn}>----</Text>
+    //         )
+    //     }else{
+    //         return(
+    //             <Text style={styles.textVn}>{word.vn}</Text>
+    //         )
+    //     }
+    // }
     render() {
-        const word = {id : 1 , en : 'One' , vn : 'Mot' , isMemorized : true}
+        const word = { id: 1, en: 'One', vn: 'Mot', isMemorized: true }
         return (
             <View style={styles.container}>
                 <View style={styles.groupText}>
-                    <Text style={styles.textEn}>{word.en}</Text>
-                    {this.showVn(word)}
+                    <Text
+                        style={styles.textEn}>
+                        {word.en}
+                    </Text>
+                    <Text
+                        style={styles.textVn}>
+                        {word.isMemorized ? "----" : word.vn}
+                    </Text>
                 </View>
-
+                <View style={styles.groupButton}>
+                   <TouchableOpacity
+                        style={{
+                            ...styles.boxMemorized,
+                            backgroundColor : word.isMemorized ? '#27A745' : '#C82333'
+                        }}
+                   >
+                       <Text 
+                            style={styles.textMemorized}>
+                            {word.isMemorized ? 'Forgot' : 'Memorized'}
+                        </Text>
+                   </TouchableOpacity>
+                   <TouchableOpacity
+                        style={styles.boxRemove}
+                   >
+                       <Text style={styles.textRemove}>Remove</Text>
+                   </TouchableOpacity>
+                </View>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
-    container : {
-        flex : 1
+    container: {
+        flex: 1,
+        backgroundColor : ''
     },
-    groupText : {
-        flexDirection : 'row',
-        justifyContent : 'space-evenly'
+    groupText: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
     },
-    textEn : {
-        color : 'green',
-        fontSize : 20
+    textEn: {
+        color: 'green',
+        fontSize: 20
     },
-    textVn : {
-        color : 'red',
-        fontSize : 20
+    textVn: {
+        color: 'red',
+        fontSize: 20
+    },
+    groupButton: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
+    textMemorized: {
+        color : 'white'
+    },
+    textRemove: {
+        color : 'black'
+    },
+    boxRemove: {
+        padding : 10,
+        backgroundColor : '#E0A800'
+    },
+    boxMemorized: {
+        padding : 10,
     }
 })
