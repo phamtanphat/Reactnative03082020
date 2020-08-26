58002c5721a454f213b04bf50276ffe639d0f754
phamtanphat <phatdroid94@gmail.com>
2020-08-26 07:05:15 -0700

    xu ly dieu kien hien thi view

--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -3,13 +3,25 @@ import { Text, View , StyleSheet} from 'react-native'
 import Box from '../components/Box'
 
 export default class MainScreen extends Component {
+
+    showVn = (word) => {
+        if(word.isMemorized){
+            return(
+                <Text style={styles.textVn}>----</Text>
+            )
+        }else{
+            return(
+                <Text style={styles.textVn}>{word.vn}</Text>
+            )
+        }
+    }
     render() {
-        const word = {id : 1 , en : 'One' , vn : 'Mot'}
+        const word = {id : 1 , en : 'One' , vn : 'Mot' , isMemorized : true}
         return (
             <View style={styles.container}>
                 <View style={styles.groupText}>
-                    <Text>{word.en}</Text>
-                    <Text>{word.vn}</Text>
+                    <Text style={styles.textEn}>{word.en}</Text>
+                    {this.showVn(word)}
                 </View>
 
             </View>
@@ -23,5 +35,13 @@ const styles = StyleSheet.create({
     groupText : {
         flexDirection : 'row',
         justifyContent : 'space-evenly'
+    },
+    textEn : {
+        color : 'green',
+        fontSize : 20
+    },
+    textVn : {
+        color : 'red',
+        fontSize : 20
     }
 })
