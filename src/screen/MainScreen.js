e9c3b1cb6dd8818870594e37c8f7ce5ce03c62bc
phamtanphat <phatdroid94@gmail.com>
2020-08-28 07:24:09 -0700

    form ui

--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, FlatList , TextInput} from 'react-native'
 import Box from '../components/Box'
 import dimensions from '../utils/dimensions'
 
@@ -10,7 +10,8 @@ export default class MainScreen extends Component {
             { id: 2, en: 'Two', vn: 'Hai', isMemorized: false },
             { id: 3, en: 'Three', vn: 'Ba', isMemorized: false },
             { id: 4, en: 'Four', vn: 'Bon', isMemorized: true },
-        ]
+        ],
+        shouldShowForm : true
     }
     toggleWord = (id) => {
         const newWords = this.state.words.map(item => {
@@ -30,9 +31,46 @@ export default class MainScreen extends Component {
         })
         this.setState({ words: newWords })
     }
+    renderForm = (shouldShowForm) => {
+        if (shouldShowForm){
+            return (
+                <View >
+                    <View style={styles.containerTextInput}>
+                    <TextInput 
+                        placeholder="English"
+                        style={styles.textInput}/>
+                    <TextInput 
+                        placeholder="Vietnamese"
+                        style={styles.textInput}/>
+                    </View>
+                    <View style={styles.containerTouchable}>
+                        <TouchableOpacity
+                            style={styles.touchableAddword}
+                        >
+                            <Text style={styles.textTouchable}>Add word</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.touchableCancel}
+                        >
+                            <Text style={styles.textTouchable}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+            );
+        } else {
+            return (
+                <TouchableOpacity
+                    style={styles.buttonOpenForm}>
+                    <Text style={styles.textOpenForm}>+</Text>
+                </TouchableOpacity>
+            )
+        }
+    }
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+                {this.renderForm(this.state.shouldShowForm)}
                 <FlatList
                     data={this.state.words}
                     extraData={this.state.words}
@@ -73,16 +111,60 @@ export default class MainScreen extends Component {
                             <View style={{height : 10}}/>
                         )
                     }}
-                    refreshing={true}
-                    onRefresh={()=>{
-                        console.log("refresh")
-                    }}
                 />
             </View >
         )
     }
 }
 const styles = StyleSheet.create({
+    container: {
+        flex : 1
+    },
+    containerTextInput: {
+        width : '100%',
+        height: 150,
+        justifyContent : 'space-evenly'
+    },
+    textInput : {
+        borderWidth : 1,
+        height: 60,
+        fontSize : 20,
+        marginHorizontal : 10,
+        paddingHorizontal: 10,
+    },
+    touchableAddword : {
+        backgroundColor : '#218838',
+        padding : 15,
+        borderRadius : 10
+    },
+    textTouchable: {
+        color : 'white',
+        fontSize : 20,
+        fontWeight : '500'
+    },
+    touchableCancel: {
+        backgroundColor : 'red',
+        padding : 15,
+        borderRadius : 10
+    },
+    buttonOpenForm: {
+        width : '100%',
+        height : 50,
+        backgroundColor : '#45B157',
+        borderRadius : 5,
+        marginBottom : 10,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    textOpenForm: {
+        color : 'white',
+        fontSize : 30
+    },
+    containerTouchable: {
+        flexDirection : 'row',
+        justifyContent : 'space-evenly',
+        marginBottom : 10
+    },
     wordgroup: {
         flexDirection: 'column',
         justifyContent: 'space-evenly',
@@ -130,8 +212,3 @@ const styles = StyleSheet.create({
         fontWeight: '500'
     },
 })
-
-/*
-
-
-*/
