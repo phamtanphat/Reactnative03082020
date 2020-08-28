d699a07215e665512f53aaeea87dbfb08ed826db
phamtanphat <phatdroid94@gmail.com>
2020-08-28 07:08:53 -0700

    flatlist

--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -1,68 +1,74 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
 import Box from '../components/Box'
 import dimensions from '../utils/dimensions'
 
 export default class MainScreen extends Component {
     state = {
-        words : [
+        words: [
             { id: 1, en: 'One', vn: 'Mot', isMemorized: true },
             { id: 2, en: 'Two', vn: 'Hai', isMemorized: false },
             { id: 3, en: 'Three', vn: 'Ba', isMemorized: false },
             { id: 4, en: 'Four', vn: 'Bon', isMemorized: true },
         ]
     }
+    toggleWord = (id) => {
+        const newWords = this.state.words.map(item => {
+            if (item.id == id) {
+                return { ...item, isMemorized: !item.isMemorized }
+            }
+            return item
+        })
+        this.setState({ words: newWords })
+    }
+    removeWord = (id) => {
+        const newWords = this.state.words.filter(item => {
+            if (item.id == id) {
+                return false
+            }
+            return true
+        })
+        this.setState({ words: newWords })
+    }
     render() {
         return (
             <View>
-                {this.state.words.map(word => {
-                    return (
-                        <View 
-                            key={word.id}
-                            style={styles.wordgroup} >
-                            <View style={styles.textgroup}>
-                                <Text style={styles.textEn}>{word.en}</Text>
-                                <Text style={styles.textVn}>
-                                    {word.isMemorized ? '----' : word.vn}
-                                </Text>
-                            </View>
-                            <View style={styles.textgroup}>
-                                <TouchableOpacity
-                                    onPress={() => {
-                                        const newWords = this.state.words.map(item =>{
-                                            if(item.id == word.id){
-                                                return {...item , isMemorized : !item.isMemorized}
-                                            }
-                                            return item
-                                        })
-                                        this.setState({words : newWords})
-                                    }}
-                                    style={word.isMemorized ? styles.buttonisForgot : styles.buttonisMemorized}
-                                >
-                                    <Text
-                                        style={styles.textisMemorized}>
-                                        {word.isMemorized ? "Forgot" : "isMemorized"}
+                <FlatList
+                    data={this.state.words}
+                    extraData={this.state.words}
+                    keyExtractor={(item, index) => item.id.toString()}
+                    renderItem={({ item }) => {
+                        return (
+                            <View
+                                key={item.id}
+                                style={styles.wordgroup} >
+                                <View style={styles.textgroup}>
+                                    <Text style={styles.textEn}>{item.en}</Text>
+                                    <Text style={styles.textVn}>
+                                        {item.isMemorized ? '----' : item.vn}
                                     </Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    onPress={() => {
-                                        const newWords = this.state.words.filter(item =>{
-                                            if(item.id == word.id){
-                                                return false
-                                            }
-                                            return true
-                                        })
-                                        this.setState({words : newWords})
-                                    }}
-                                    style={styles.buttonRemove}
-                                >
-                                    <Text style={styles.textRemove}>Remove</Text>
-                                </TouchableOpacity>
+                                </View>
+                                <View style={styles.textgroup}>
+                                    <TouchableOpacity
+                                        onPress={() => this.toggleWord(item.id)}
+                                        style={item.isMemorized ? styles.buttonisForgot : styles.buttonisMemorized}
+                                    >
+                                        <Text
+                                            style={styles.textisMemorized}>
+                                            {item.isMemorized ? "Forgot" : "isMemorized"}
+                                        </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={() => this.removeWord(item.id)}
+                                        style={styles.buttonRemove}
+                                    >
+                                        <Text style={styles.textRemove}>Remove</Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
-                        </View>
-                    )
-                })
-                }
+                        )
+                    }}
+                />
             </View >
         )
     }
@@ -74,7 +80,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0F0F0',
         borderRadius: 5,
         paddingVertical: 5,
-        margin : 10
+        margin: 10
     },
     textgroup: {
         flexDirection: 'row',
