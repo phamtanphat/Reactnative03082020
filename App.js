a9742058b4ee15030c6eb9a9da9e4f78e9ead132
phamtanphat <phatdroid94@gmail.com>
2020-09-11 21:03:47 +0700

    gan chuc nang cho reducer

--- App.js
+++ App.js
@@ -21,15 +21,39 @@ function wordReducer(state = worddefault, action) {
   if (action.type === 'ADD_WORD') {
     return action.words;
   }
+  if (action.type === 'TOGGLE_WORD') {
+    const newWords = state.map((item) => {
+      if (item.id === action.id) {
+        return {...item, isMemorized: !item.isMemorized};
+      }
+      return item;
+    });
+    return newWords;
+  }
+  if (action.type === 'REMOVE_WORD') {
+    const newWords = state.filter((item) => {
+      if (item.id === action.id) {
+        return false;
+      }
+      return true;
+    });
+    return newWords;
+  }
   return state;
 }
 function shouldShowFormReducer(state = false, action) {
   if (action.type === 'ADD_WORD') {
     return !state;
   }
+  if (action.type === 'TOGGLE_FORM') {
+    return !state;
+  }
   return state;
 }
 function filterModeReducer(state = 'Show_All', action) {
+  if (action.type === 'SET_FILTER_MODE') {
+    return action.filterMode;
+  }
   return state;
 }
 
@@ -52,31 +76,4 @@ export default class App extends Component {
     );
   }
 }
-// if (action.type === 'TOGGLE_WORD') {
-//   const newWords = state.words.map((item) => {
-//     if (item.id === action.id) {
-//       return {...item, isMemorized: !item.isMemorized};
-//     }
-//     return item;
-//   });
-//   return {...state, words: newWords};
-// }
-// if (action.type === 'REMOVE_WORD') {
-//   const newWords = state.words.filter((item) => {
-//     if (item.id === action.id) {
-//       return false;
-//     }
-//     return true;
-//   });
-//   return {...state, words: newWords};
-// }
-// if (action.type === 'TOGGLE_FORM') {
-//   return {...state, shouldShowForm: !state.shouldShowForm};
-// }
-// if (action.type === 'ADD_WORD') {
-//   return {...state, words: action.words};
-// }
-// if (action.type === 'SET_FILTER_MODE') {
-//   return {...state, filterMode: action.filterMode};
-// }
-// return state;
+
